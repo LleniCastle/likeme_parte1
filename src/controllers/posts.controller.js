3547c3435d42ac3dc14a1a45a22180e924cfe034
LleniCastle <129088819+LleniCastle@users.noreply.github.com>
2024-06-19 15:37:39 -0400

    se envía el array de posts y newpost directamente. posts.controller.js

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -3,7 +3,7 @@ import { createPostModel, getAllPostModel } from '../models/posts.model.js';
 export const getAllPost = async (req, res) => {
     try {
         const posts = await getAllPostModel();
-        res.status(200).json({ posts: posts });
+        res.status(200).json(posts);
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
@@ -13,7 +13,7 @@ export const createPost = async (req, res) => {
     try {
         const { post } = req.body;
         const newPost = await createPostModel(post);
-        res.status(201).json({ post: newPost });
+        res.status(201).json(newPost);
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
